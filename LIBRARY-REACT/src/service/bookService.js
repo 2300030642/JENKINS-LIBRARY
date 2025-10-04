57c9f3d0729fd40fc956baa2a0696fd6705da8e0
Nishitha <nishithanishitha3434@gmail.com>
2025-10-04 12:15:16 +0530

    full stack v1.0

--- LIBRARY-REACT/src/service/bookService.js
+++ LIBRARY-REACT/src/service/bookService.js
@@ -3,7 +3,17 @@ import config from "./config";
 
 const BASE_URL = config.BASE_URL;
 
+// Get status message ("/")
+export const getApiStatus = () => axios.get(`${BASE_URL}/`);
+
+// Get all books
 export const getBooks = () => axios.get(`${BASE_URL}/all`);
+
+// Add a book
 export const addBook = (book) => axios.post(`${BASE_URL}`, book);
+
+// Update a book by ID
 export const updateBook = (id, book) => axios.put(`${BASE_URL}/${id}`, book);
+
+// Delete a book by ID
 export const deleteBook = (id) => axios.delete(`${BASE_URL}/${id}`);
