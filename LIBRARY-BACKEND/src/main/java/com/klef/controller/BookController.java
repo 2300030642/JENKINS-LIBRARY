042248ea7771266d4aa17b455a3bcd4ca4754e3a
Nishitha <nishithanishitha3434@gmail.com>
2025-10-14 23:58:58 +0530

    updated

--- LIBRARY-BACKEND/src/main/java/com/klef/controller/BookController.java
+++ LIBRARY-BACKEND/src/main/java/com/klef/controller/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/springbootlibraryapi")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Autowired
