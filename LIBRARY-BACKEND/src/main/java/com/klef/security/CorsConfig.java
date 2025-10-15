9ede23c648e7b115c0cdb3f0ad96f316c10d5bec
Nishitha <nishithanishitha3434@gmail.com>
2025-10-16 02:44:23 +0530

    updated

--- LIBRARY-BACKEND/src/main/java/com/klef/security/CorsConfig.java
+++ LIBRARY-BACKEND/src/main/java/com/klef/security/CorsConfig.java
@@ -12,10 +12,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        registry.addMapping("/**")
+    .allowedOriginPatterns("*")
+    .allowedMethods("*")
+    .allowedHeaders("*");
+
             }
         };
     }
