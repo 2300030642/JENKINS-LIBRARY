4857e41b63363700bff943b0a5201efd5e06ea21
Nishitha <nishithanishitha3434@gmail.com>
2025-10-16 04:28:05 +0530

    updated

--- LIBRARY-REACT/src/service/config.js
+++ LIBRARY-REACT/src/service/config.js
@@ -1,4 +1,4 @@
 const config = {
-    BASE_URL: "http://localhost:2030/springbootlibraryapi/springbootlibraryapi"
+    BASE_URL: "http://localhost:2030/springbootlibraryapi"
 }
 export default config;
